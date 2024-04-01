7c41dcf8c46b74c182da8ed6a25c7bd88204e23f
never130 <never130@hotmail.com>
2024-04-01 09:55:47 -0300

    Realizamos servidor web similar al pajaro

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const { envs } = require('./config/env')
+const { startServer } = require('./server/server')
+
+
+const main = () => {
+    startServer({
+        port: envs.PORT,
+        public_path: envs.PUBLIC_PATH
+   })
+}
+
+
+// Funcion agnostica autoconvocada
+// agnostica porque no tiene nombre
+// autoconvocada porque la ejecutamos con los parentesis
+(async () => {
+    main()
+})()
+
+
